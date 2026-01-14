0dd4a8ea8c6ced65af83655d1d92b7214e615194
Profe-Teo-Torre7 <teo_torre7@proton.me>
2026-01-14 20:25:40 +0100

    dia 14 enero

--- prueba.js
+++
@@ -1,8 +0,0 @@
-        function toString(lista){ // Devuelve una cadena con los elementos de la lista
-            let salida = '';
-            for(let i = 0; i < lista.length; i++){
-                salida += lista[i];
-            }
-            return salida;
-        }
-toString('Hola caracola')
