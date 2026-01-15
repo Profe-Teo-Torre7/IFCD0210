58d276d42297066f952157f9a796450fbdf4bb02
Profe-Teo-Torre7 <teo_torre7@proton.me>
2026-01-15 20:26:34 +0100

    Día 15 enero

---
+++ UF1842/elements.js
@@ -0,0 +1,13 @@
+function agregar() {
+    let padre = document.getElementById("lista");
+    //let padre = document.querySelector("#lista");
+    let li = document.createElement("li");
+    li.textContent = "Elemento nuevo";
+    padre.appendChild(li);
+}
+function eliminar() {
+    let padre = document.getElementById("lista");
+    let li = padre.lastChild;
+    //console.log(li);
+    if (li) { padre.removeChild(li) };
+}
