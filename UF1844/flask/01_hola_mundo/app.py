ef14aee184ceee458ca3847b2cd75765cb672adf
Profe-Teo-Torre7 <teo_torre7@proton.me>
2026-02-05 20:21:09 +0100

    Dia 5 feb

--- UF1844/flask/01_hola_mundo/app.py
+++ UF1844/flask/01_hola_mundo/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -32,5 +32,45 @@ def prueba():
     return render_template('prueba.html')
 
 
+@app.route('/prueba1/')
+@app.route('/prueba1/<string:nombre>')
+@app.route('/prueba1/<string:nombre>/<int:numero>')
+def saludo(nombre=None,numero=None):
+    salida = ""
+    if nombre and numero:
+        salida = f"Hola {nombre} tienes {numero} años (pero pareces mayor)"
+    elif nombre:
+        salida = f"Hola {nombre}"
+    else:
+        salida = "Hola caracola"
+    
+    return salida
+
+
+@app.route('/suma',methods=['GET','POST'])
+def sumar():
+    if request.method == 'POST':
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        return str(int(num1) + int(num2))
+    else:
+        cadena = '''
+        <form action="/suma" method="post">
+
+            <label>N1:</label>
+            <input type="text" name="num1"/>
+
+            <label>N2:</label>
+            <input type="text" name="num2"/>
+
+            <input type="submit" value="Sumar"/>
+        </form>
+        '''
+        return cadena
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
