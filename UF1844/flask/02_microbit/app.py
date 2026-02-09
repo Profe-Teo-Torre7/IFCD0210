7d49071be3b4f0353a53243a90a4279599409930
Profe-Teo-Torre7 <teo_torre7@proton.me>
2026-02-09 20:28:36 +0100

    Dia 9 feb

--- UF1844/flask/02_microbit/app.py
+++ UF1844/flask/02_microbit/app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 def hola():
     return render_template('home.html')
 
+@app.route('/software')
+def get_software():
+    return render_template('software.html')
+
+@app.route('/firmware')
+def get_firmware():
+    return render_template('firmware.html')
