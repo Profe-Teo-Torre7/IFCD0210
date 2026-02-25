b1c279359eeb205d725b90f35de5de37305f41cc
Profe-Teo-Torre7 <teo_torre7@proton.me>
2026-02-25 18:12:37 +0100

    Dia 25 Febrero

--- UF1845/consultas_jardineria_1.sql
+++ UF1845/consultas_jardineria_1.sql
@@ -314,7 +314,122 @@ where
 	where c.codigo_empleado_rep_ventas is null
 	
 	
+-- Consultas resumen
+-- --------------------
+
+-- 1. ¿Cuántos empleados hay en la compañía?
 	
+	select count(*) as num_empleados
+	from empleado e 
+	
+-- 2. ¿Cuántos clientes tiene cada país?
+	
+	select c.pais ,count(*) as total
+	from cliente c 
+	group by c.pais
+	
+	
+-- 3. ¿Cuál fue el pago medio en 2009?
+	
+	select avg(p.total) as promedio 
+	from pago p
+	where year(p.fecha_pago ) = "2009"
+	
+	select year(p.fecha_pago ) as annio,
+	avg(p.total) as promedio 
+	from pago p
+	group by annio 
+	order by annio desc
+	limit 1
+
+	
+	select year(p.fecha_pago ) as annio,
+	avg(p.total) as promedio 
+	from pago p
+	where year(p.fecha_pago ) in (2006,2009,2008)
+	group by annio 
+	
+-- 4. ¿Cuántos pedidos hay en cada estado? Ordena el resultado de forma descendente por el número de pedidos.
+	
+	select count(*) as total_pedidos, estado
+	from pedido
+	group by estado
+	order by total_pedidos desc
+	
+	
+-- 5. Calcula el precio de venta del producto más caro y más barato en una misma consulta.
+	
+	select max(p.precio_venta) as maximo,
+	min(p.precio_venta) as minimo
+	from producto p 
+	
+	-- Producto más rentable
+	
+	select
+	p.codigo_producto,
+	p.nombre ,
+	(p.precio_venta -p.precio_proveedor) as margen
+from
+	producto p
+order by
+	margen desc
+limit 1
+	
+	
+-- 6. Calcula el número de clientes que tiene la empresa.
+
+select count(*) as total_clientes
+from cliente c 
+
+
+-- 7. ¿Cuántos clientes existen con domicilio en la ciudad de Madrid?
+
+select count(*) from cliente c 
+where c.ciudad ="Madrid"
+
+-- 8. ¿Calcula cuántos clientes tiene cada una de las ciudades que empiezan por M?
+
+select
+	c.ciudad,
+	count(*) as total
+from
+	cliente c
+where
+	c.ciudad like "M%"
+group by
+	c.ciudad
+order by
+	total desc
+
+-- 9. Devuelve el nombre de los representantes de ventas y el número de clientes al que atiende cada uno.
+	
+	SELECT
+	e.nombre ,
+	count(c.codigo_cliente) as total
+from
+	empleado e
+join cliente c 
+	on
+	e.codigo_empleado = c.codigo_empleado_rep_ventas
+group by
+	e.nombre
+order by
+	total desc
+	
+	
+-- 10. Calcula el número de clientes que no tiene asignado representante de ventas.
+-- 11. Calcula la fecha del primer y último pago realizado por cada uno de los clientes. El listado deberá mostrar el nombre y los apellidos de cada cliente.
+-- 12. Calcula el número de productos diferentes que hay en cada uno de los pedidos.
+-- 13. Calcula la suma de la cantidad total de todos los productos que aparecen en cada uno de los pedidos.
+
+-- 14. Devuelve un listado de los 20 productos más vendidos y el número total de unidades que se han vendido de cada uno. El listado deberá estar ordenado por el número total de unidades vendidas.
+-- 15. La facturación que ha tenido la empresa en toda la historia, indicando la base imponible, el IVA y el total facturado. La base imponible se calcula sumando el coste del producto por el número de unidades vendidas de la tabla detalle_pedido. El IVA es el 21 % de la base imponible, y el total la suma de los dos campos anteriores.
+-- 16. La misma información que en la pregunta anterior, pero agrupada por código de producto.
+-- 17. La misma información que en la pregunta anterior, pero agrupada por código de producto filtrada por los códigos que empiecen por OR.
+-- 18. Lista las ventas totales de los productos que hayan facturado más de 3000 euros. Se mostrará el nombre, unidades vendidas, total facturado y total facturado con impuestos (21% IVA).
+-- 19. Muestre la suma total de todos los pagos que se realizaron para cada uno de los años que aparecen en la
+tabla pagos.
+
 
 
 
