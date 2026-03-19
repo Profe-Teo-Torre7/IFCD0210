f39f81c2f413c71c632c3a809b58e0dbada42f66
Profe-Teo-Torre7 <teo_torre7@proton.me>
2026-03-19 17:34:43 +0100

    Dia 19 Marzo

--- UF1846/flask_blog/c_negocio/app.py
+++ UF1846/flask_blog/c_negocio/app.py
@@ -35,6 +35,14 @@ def update_post_db(post_id,data):
 def delete_post_db(post_id):
     resp = requests.delete(f"{RUTA_DATOS}/post/{post_id}")
 
+def validar_post(data):
+    required = ['titulo','contenido','id_autor','estado']
+    for campo in required:
+        if not campo in data:
+            return False
+
+    return True
+
 # -------------------------------------------
 
 @app.route("/api/login",methods=['POST'])
@@ -54,6 +62,8 @@ def login():
         'rol': user['rol']}
     )
 
+
+
 # ---------------------------------
 
 @app.route('/api/posts')
@@ -68,3 +78,53 @@ def get_posts():
     return jsonify(publicados)
 
 
+@app.route('/api/posts_all')
+def get_posts_all():
+    posts = get_posts_db()    
+    return jsonify(posts)
+
+
+@app.route('/api/post/<int:post_id>')
+def get_post(post_id):
+    post = get_post_db(post_id)
+    if not post or post['estado'] != 'publicado':
+        return jsonify({'error':'No encontrado'}),404
+    
+    return jsonify(post)
+
+
+@app.route('/api/posts', methods=['POST'])
+def create_post():
+    data = request.json
+
+    if validar_post(data):
+        post = {
+        'titulo':    data['titulo'],
+        'contenido': data['contenido'],
+        'id_autor':  data['id_autor'],
+        'estado':    data['estado']
+        }  
+        resultado = create_post_db(post)
+        return jsonify(resultado)
+    
+    return jsonify({'error': 'Faltan campos obligatorios'}),400
+
+
+@app.route('/api/post/<int:post_id>' , methods=['PUT'])
+def update_post(post_id):
+    data = request.json
+    if validar_post(data):
+        update_post_db(post_id,data)
+        return jsonify({'mensaje':'Post actualizado.'})
+    
+    return jsonify({'error': 'Faltan campos obligatorios'}),400
+
+
+@app.route('/api/post/<int:post_id>',methods=['DELETE'])
+def delete_post(post_id):
+    delete_post_db(post_id)
+    return jsonify({'mensaje':'Post eliminado.'})
+
+#------------------------------------------------
+if __name__ == '__main__':
+    app.run(port=5002,debug=True)
